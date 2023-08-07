6e504171ce2404fabd64aed5a618abea7763a7c0
janded-k <97323746+janded-k@users.noreply.github.com>
2023-08-07 15:52:12 +0100

    second commit

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 10 seconds
+  setTimeout(showSlides, 7000); // Change image every 7 seconds
 }
 
 function openForm(){
